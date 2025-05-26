03c6ef14eb6e55ebc16cbdc6629e9f143dafcf49
NikitaK <kuranovnikita@gmail.com>
2025-05-26 22:14:43 +0400

    'suite'

---
+++ tests/suite/suite.go
@@ -0,0 +1,64 @@
+package suite
+
+import (
+	"context"
+	"ecomUser/internal/config"
+	"net"
+	"strconv"
+	"testing"
+
+	user1 "github.com/KuranovNikita/ecomProto/gen/go/user"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Suite struct {
+	*testing.T
+	Cfg        *config.Config
+	AuthClient user1.UserServiceClient
+}
+
+const (
+	grpcHost = "localhost"
+)
+
+func New(t *testing.T) (context.Context, *Suite) {
+	t.Helper()
+	t.Parallel()
+
+	cfg := config.MustLoad()
+
+	grpcRequestTimeout := cfg.GRPCTimeout
+
+	ctx, cancelCtx := context.WithTimeout(context.Background(), grpcRequestTimeout)
+
+	t.Cleanup(func() {
+		t.Helper()
+		cancelCtx()
+	})
+
+	cc, err := grpc.NewClient(
+		grpcAddress(cfg),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		t.Fatalf("grpc server connection failed: %v", err)
+	}
+
+	t.Cleanup(func() {
+		if cc != nil {
+			cc.Close()
+		}
+	})
+
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: user1.NewUserServiceClient(cc),
+	}
+
+}
+
+func grpcAddress(cfg *config.Config) string {
+	return net.JoinHostPort(grpcHost, strconv.Itoa(cfg.GRPCPort))
+}
